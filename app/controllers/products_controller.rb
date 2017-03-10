143e8cc76add3f51319bfbaa785e34fc749db227
Olie Bylka <oabylova@gmail.com>
2017-03-09 21:18:56 -0800

    Commiting changes to mini cap: creating a form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,17 @@ class ProductsController < ApplicationController
 	def view_products
 		@products = Product.all
 	end
+
+	def new_product_form
+		
+	end
+
+	def new_product
+		input_name = params[:name]
+		input_price = params[:price]
+		input_image = params[:image]
+		input_desc = params[:description]
+
+		@product = Product.create(name:input_name , price:input_price , image:input_image , description:input_desc)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/view_products" => "products#view_products"
 
+  get "/new_product_form" => "products#new_product_form"
+  post "/new_product" => "products#new_product"
+
 end
