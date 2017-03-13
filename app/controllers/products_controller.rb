a2bf9ef40024f5f818eae905d868f7b34218ef7b
Olie Bylka <oabylova@gmail.com>
2017-03-13 09:11:13 -0700

    Added images, some html and css, edited views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,18 @@
 class ProductsController < ApplicationController
-	def view_products
+	def index
 		@products = Product.all
 	end
 
-	def new_product_form
+	def new
 		
 	end
 
-	def new_product
+	def show
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
+	end
+
+	def create
 		input_name = params[:name]
 		input_price = params[:price]
 		input_image = params[:image]
@@ -15,4 +20,39 @@ class ProductsController < ApplicationController
 
 		@product = Product.create(name:input_name , price:input_price , image:input_image , description:input_desc)
 	end
+
+	def update
+		input_name = params[:name]
+		input_price = params[:price]
+		input_image = params[:image]
+		input_desc = params[:description]
+
+		product = Product.find_by(id: params[:id])
+		@product = product.update(name:input_name , price:input_price , image:input_image , description:input_desc)
+		#@product.save
+
+		redirect_to "/products/#{product.id}"
+	end
+
+	def edit
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to '/products'
+	end
+
+	def search_results
+		@products = Product.where("name LIKE '%#{params[:keywords]}%'")
+	end
 end
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/view_products" => "products#view_products"
 
-  get "/new_product_form" => "products#new_product_form"
-  post "/new_product" => "products#new_product"
+ #index
+ get "/products" => "products#index"
+
+# New and Create
+ get "/products/new" => "products#new"
+ post "/products" => "products#create"
+
+# Show
+ get "/products/:id" => "products#show"
+
+#Edit and Updates
+ get "/products/:id/edit" => "products#edit"
+ patch "/products/:id" => "products#update"
+
+# Destroy
+ delete "/products/:id" => "products#destroy"
+
+#Search Results
+ post "/products/search_results" => "products#search_results"
 
 end
