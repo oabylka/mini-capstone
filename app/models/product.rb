a8eeb3993edd0b0e7e7b66dbe572cfe2c673221f
Olie Bylka <oabylova@gmail.com>
2017-03-14 20:52:01 -0700

    added model methods and conditional styling to price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,23 @@
 class Product < ApplicationRecord
 
+	def sale_message(price)
+		price = price
+		if price < 2
+			"Discount item!"
+		else
+			"Everyday Value!"
+		end
+	end
+
+	def tax(price)
+		price = price
+		(price*0.09).round(2)
+	end
+
+	def total(price, tax)
+		price = price
+		tax = tax
+		price+tax
+	end
+
 end
